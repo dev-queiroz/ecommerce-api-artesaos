8d4bbe30a6f824edae3c939bb4c3a4c332124b32
dev-queiroz <queirozdouglas466@gmail.com>
2025-06-21 11:36:58 -0300

    Adiciona documentação Swagger para endpoints de produtos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {createProduto, getProdutos, uploadMiddleware} from './controllers/produtos';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../docs/api-docs.yaml';
+import yaml from 'yamljs';
 
 dotenv.config();
 
@@ -13,6 +16,9 @@ app.get('/', (req, res) => {
 
 app.get('/produtos', getProdutos);
 app.post('/produtos', uploadMiddleware, createProduto);
+
+// Configuração do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(swaggerDocument)));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
